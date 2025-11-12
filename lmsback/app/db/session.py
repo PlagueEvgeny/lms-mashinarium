7f9dbe0af3da3b03d65d0e523799b2c54ed51b5c
PlagueEvgeny <PlagueEvgeny@yandex.ru>
2025-11-12 22:10:02 +0500

    1. Modified connection db

--- lmsback/app/db/session.py
+++ lmsback/app/db/session.py
@@ -16,3 +16,11 @@ engine = create_async_engine(config.REAL_DATABASE_URL, future=True, echo=True)
 # create session for the interaction with database
 
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+async def get_db() -> Generator:
+    try:
+        session: AsyncSession = async_session()
+        yield session
+
+    finally:
+        await session.close()
