85c1c51a20efd277bb03393ddf9dd2ca86a6c8fe
PlagueEvgeny <PlagueEvgeny@yandex.ru>
2025-12-16 14:02:34 +0500

    Add check permmission access all but User

--- lmsback/app/services/user_service.py
+++ lmsback/app/services/user_service.py
@@ -7,7 +7,7 @@ from uuid import UUID
 
 from decimal import Decimal
 
-from sqlalchemy import and_, extract
+from sqlalchemy import and_
 from sqlalchemy import select
 from sqlalchemy import update
 from sqlalchemy.ext.asyncio import AsyncSession
