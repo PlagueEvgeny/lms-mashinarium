5b9a1e4105119c997217ea13c5434ca8f4faedb5
PlagueEvgeny <PlagueEvgeny@yandex.ru>
2026-01-27 14:58:12 +0500

    Create testing version models lessons for lecture

---
+++ lmsback/app/db/models/lesson.py
@@ -0,0 +1,43 @@
+from db import Base
+
+class LessonType(str, Enum):
+    LECTURE = "lecture"
+    VIDEO = "video"
+    PRACTICA = "practica"
+    TEST = "test"
+
+
+class LessonBase(Base):
+    __tablename__ = "lessons"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    module_id = Column(Integer, ForeignKey("modules.id"))
+    name = Column(String, nullable=False)
+    slug = Column(String, nullable=False, unique=True)
+    description = Column(Text, nullable=True)
+    display_order = Column(Integer, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    is_active = Column(Boolean, default=True)
+    lesson_type = Column(String, nullable=False)
+    
+    __mapper_args__ = {
+        'polymorphic_on': lesson_type,
+        'polymorphic_identity': 'lesson'
+    }
+        
+    modules = relationship("Module", back_populates="lessons")
+
+
+class Lecture(LessonBase):
+    __tablename__ = "lectures"
+
+    id = Column(Integer, ForeignKey("lessons.id"), primary_key=True)
+    content = Column(Text, nullable=False)
+    images = Column(JSON)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'lecture'
+    }
+
+
